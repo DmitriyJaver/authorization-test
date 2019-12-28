e8ccc027dd5ce9d92cc2fa8664ad501bd69ab91e
Aleksey Shevchuk <shevchuk.a@gmail.com>
2019-12-28 20:02:28 +0200

    Revert "create table Token and modify User table"
    This reverts commit 3c111ae7

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,8 +20,6 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->string('country_code', 4)->nullable();
-            $table->string('phone')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
