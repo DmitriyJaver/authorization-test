1e94a3cfa51b7ac4dfd19cc6c0af5cb8d278e676
Aleksey Shevchuk <shevchuk.a@gmail.com>
2019-12-28 20:02:08 +0200

    Revert "Midify Login and Token Controllers"
    This reverts commit 2ca96d1c

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,10 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
-use App\Token;
-use http\Env\Request;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
-use Illuminate\Support\Facades\Lang;
 
 class LoginController extends Controller
 {
@@ -39,36 +36,5 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
-    }
-
-    public function Login(Request $request)
-    {
-        $this->validateLogin($request);
-
-        //retrieveByCredentials
-        if ($user = app('auth')->getProvider()->retrieveByCredentials($request->only('email', 'password'))) {
-            $token = Token::create([
-                'user_id' => $user->id
-            ]);
-
-            if ($token->sendCode()) {
-                session()->set("token_id", $token->id);
-                session()->set("user_id", $user->id);
-                session()->set("remember", $request->get('remember'));
-
-                return redirect("code");
-            }
-
-            $token->delete();// delete token because it can't be sent
-            return redirect('/login')->withErrors([
-                "Unable to send verification code"
-            ]);
-        }
-
-        return redirect()->back()
-            ->withInputs()
-            ->withErrors([
-                $this->username() => Lang::get('auth.failed'),
-            ]);
     }
 }
